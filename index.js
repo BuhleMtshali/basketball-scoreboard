6bb81e6b4d6fe58831a974f0f2eb9223d65d32a1
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-05-03 13:37:33 +0200

    Updated the html, stylesheet and the  javascript file

--- index.js
+++ index.js
@@ -10,3 +10,25 @@ function increment(event) {
 
 let buttonElement = document.querySelector(".add-one-home");
 buttonElement.addEventListener("click", increment);
+
+function incrementTwice(event) {
+  event.preventDefault();
+  count = count + 2;
+  let scoreHeading = document.querySelector("#score-home");
+
+  scoreHeading.innerHTML = count;
+}
+
+let buttonTwoElement = document.querySelector(".add-two-home");
+buttonTwoElement.addEventListener("click", incrementTwice);
+
+function incrementThree(event) {
+  event.preventDefault();
+  count = count + 3;
+  let scoreHeading = document.querySelector("#score-home");
+
+  scoreHeading.innerHTML = count;
+}
+
+let buttonThreeElement = document.querySelector(".add-three-home");
+buttonThreeElement.addEventListener("click", incrementThree);
